5c20f25b8f8a73a989db8eebe9497e339900579e
m23project <hhabermann@pc-kiel.de>
2016-01-22 23:39:18 +0100

    mdk-devel-backup from 2016-01-22 23:39

--- mdk/doc/manual/bin/checkForMissingHlp.sh
+++ mdk/doc/manual/bin/checkForMissingHlp.sh
@@ -1,34 +1,44 @@
 #!/bin/bash
 
-find /m23/inc/help -type f -printf "%f\n" | grep -v "~$" |sort -u > /tmp/i18nAllFiles
+# Create a file with all .hlp and .inc files from /m23/inc/help
+find /m23/inc/help -type f -printf "%f\n" | grep -v "~$" |sort -u > /tmp/helpAllFiles
+
+# Log file to store the output messages
+logFile="/tmp/allMissingChangedm23Help.txt"
+rm $logFile 2> /dev/null
+
+# File to store the file names of changed and missing .hlp and .inc files
+tarList="/tmp/allMissingChangedm23HelpFiles.txt"
+rm $tarList 2> /dev/null
 
 
 
-
-
-
-
-#get all directories with
-for i18nfile in `cat /tmp/i18nAllFiles`
+# Find .hlp/.inc files that have too lesse I18N_ symbols
+for helpFile in `cat /tmp/helpAllFiles`
 do
 	changed=0
 
+	# Language directories (e.g. /m23/inc/help/de)
 	for dir in `find /m23/inc/help/* -type d`
 	do
-		curI18Nfile="$dir/$i18nfile"
+		curhelpFile="$dir/$helpFile"
+
+		# 2 char language (e.g. de)
 		lang=`basename $dir`
 
-		if [ -e "$curI18Nfile" ]
+		if [ -e "$curhelpFile" ]
 		then
-			sed 's/$[A-Za-z_18]*/\n&\n/g' < "$curI18Nfile" | grep I18N | sort -u > /tmp/$lang.i18ns
+			sed 's/$[A-Za-z_18]*/\n&\n/g' < "$curhelpFile" | grep I18N | sort -u > /tmp/$lang.i18ns
 		else
-			echo "missing: $curI18Nfile"
+			echo "missing: $curhelpFile" | tee -a $logFile
 			changed=1
 		fi
 	done
 
+	# Get a list of all I18N_ symbols in all languages
 	sort -u /tmp/*.i18ns > /tmp/all.i18ns
 
+	# Check for missing I18N_ symbols in the .hlp/.inc files for a given language
 	for i18NsymbolFile in `ls /tmp/??.i18ns`
 	do
 		diff /tmp/all.i18ns $i18NsymbolFile | grep "<" | cut -d' ' -f2 > /tmp/missing.i18ns
@@ -37,10 +47,10 @@ do
 
 		if [ `wc -l /tmp/missing.i18ns | cut -d' ' -f1` -gt 0 ]
 		then
-			echo -n "$lang/$i18nfile missing: "
+			echo -n "$lang/$helpFile missing: "
 			awk -v ORS=' ' '{print}' < /tmp/missing.i18ns
 			echo "
-	cmd: kate -u /m23/inc/help/$lang/$i18nfile"
+	cmd: kate -u /m23/inc/help/$lang/$helpFile" | tee -a $logFile
 			changed=1
 		fi
 		rm /tmp/missing.i18ns
@@ -53,46 +63,68 @@ do
 	fi
 done
 
-rm /tmp/i18nAllFiles
 
 
 
 
-
-#get all directories with
-# for dir in `find /m23/inc/help/* -type d`
-# do
-# 	find $dir -type f -printf "%f\n" | grep -v "~$" |sort -u > /tmp/i18nFiles
-# 	for f in `diff /tmp/i18nAllFiles /tmp/i18nFiles | grep "<" | cut -d' ' -f2`
-# 	do
-# 		echo "$dir: $f"
-# 	done
-# done
-# 
-# rm /tmp/i18nAllFiles
-
-
-exit
-
-rm /tmp/*.m23index 2> /dev/null
-
-for file in `cat /tmp/i18nAllFiles`
+# Get a list of comments for each language
+for file in `cat /tmp/helpAllFiles`
 do
+	# Language directories (e.g. /m23/inc/help/de)
 	for dir in `find /m23/inc/help/* -type d`
 	do
-		bn=`basename $dir`
+		# 2 char language (e.g. de)
+		lang=`basename $dir`
 
 		if [ -f "$dir/$file" ]
 		then
-			sed 's/<!--/\n<!--/g' "$dir/$file" | sed 's/-->/-->\n/g' | grep "<\!--" | awk -v file=$file '{print(file":"$0)}' >> /tmp/$bn.m23index
-			#echo "$file: $md5" >> /tmp/$bn.m23index
+			# Pull out the comments
+			sed 's/<!--/\n<!--/g' "$dir/$file" | sed 's/-->/-->\n/g' | grep "<\!--" | awk -v file=$file '{print(file":"$0)}' >> /tmp/$lang.m23Comments
 		else
-			echo "$file: missing" >> /tmp/$bn.m23index
+			echo "$file: missing" >> /tmp/$lang.m23Comments | tee -a $logFile
 		fi
 	done
 done
 
-rm /tmp/i18nAllFiles
 
-exit
 
+
+
+# Check for missing comments
+for lang in en fr
+do
+	diffFile="/tmp/$lang.m23commentsMissing"
+
+	# Search comments that are missing in comparison to the German version
+	diff /tmp/$lang.m23Comments /tmp/de.m23Comments  | grep "> " | cut -d' ' -f2 | cut -d':' -f1 | sort -u > $diffFile
+
+	# Show the files with missing comments
+	if [ $(wc -l $diffFile | cut -d' ' -f1) -gt 0 ]
+	then
+		echo "Comments missing in:" | tee -a $logFile
+		awk -v dir="/m23/inc/help/$lang" '{print(dir"/"$0)}' $diffFile | tee -a $logFile
+	fi
+done
+
+
+
+
+
+# Create a file that stores the file names of changed and missing .hlp and .inc files
+cat $logFile | sed 's/ /\n/g' | while read file
+do
+	if [ -f "$file" ]
+	then
+		echo $file >> "$tarList.tmp"
+		echo $file | sed 's#/\(fr\|en\)/#/de/#' >> "$tarList.tmp"
+	fi
+done
+
+echo $logFile > $tarList
+sort -u "$tarList.tmp" >> $tarList
+rm "$tarList.tmp"
+
+tar cfvz /tmp/allMissingChangedm23HelpFiles.tar.gz -T /tmp/allMissingChangedm23HelpFiles.txt
+
+
+rm /tmp/helpAllFiles /tmp/??.m23Comments /tmp/??.m23commentsMissing /tmp/??.m23index /tmp/??.i18n 2> /dev/null
