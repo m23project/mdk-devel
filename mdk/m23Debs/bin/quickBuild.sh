159d84c8af43686bd751f1bd2017eb12046c2e0d
m23project <hhabermann@pc-kiel.de>
2016-04-05 22:21:39 +0200

    mdk-devel-backup from 2016-04-05 22:21

--- mdk/m23Debs/bin/quickBuild.sh
+++ mdk/m23Debs/bin/quickBuild.sh
@@ -34,6 +34,10 @@ case $1 in
 	"m23-ucs-extra")
 		finishBuilding m23-ucs-extra
 		updatePackages
+		# Move the package to /mdk/ucs/debs, so it will net get installed by default
+		mkdir -p /mdk/ucs/debs
+		rm /mdk/ucs/debs/m23-ucs-extra*
+		mv /mdk/server/iso/pool/m23-ucs-extra* /mdk/ucs/debs
 		exit;;
 esac
 
