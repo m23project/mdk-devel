4c32ea6ce5030c3c0958df69ea35940d38f32909
m23project <hhabermann@pc-kiel.de>
2017-01-26 21:15:27 +0100

    mdk-devel-backup from 2017-01-26 21:15

--- mdk/m23helper/dem23Client.sh
+++ mdk/m23helper/dem23Client.sh
@@ -5,3 +5,5 @@ find /etc/init.d/ | grep m23 | xargs -n1 rm
 sed -i 's#^Acquire#//Acquire#g' /etc/apt/apt.conf.d/70debconf
 
 apt-get -y --force-yes purge m23-initscripts m23hwscanner
+
+sed -i -e '/m23debs/d' -e '/extraDebs/d' -e '/hwsetup/d' /etc/apt/sources.list
