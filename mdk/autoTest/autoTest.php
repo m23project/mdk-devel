ec09f403bae4d0c0d27c807321485433e7483f74
m23project <hhabermann@pc-kiel.de>
2018-08-11 01:27:45 +0200

    mdk-devel-backup from 2018-08-11 01:27

--- mdk/autoTest/autoTest.php
+++ mdk/autoTest/autoTest.php
@@ -4,13 +4,20 @@
 include('/m23/inc/db.php');
 include('/m23/inc/vm.php');
 include('/m23/inc/helper.php');
+include("/m23/inc/i18n.php");
 include('/m23/inc/client.php');
 include('/m23/inc/checks.php');
 include('/m23/inc/autoTest.php');
 include('/m23/inc/CAutoTest.php');
+include('/m23/inc/CMessageManager.php');
+include('/m23/inc/CChecks.php');
+include('/m23/inc/CSystemProxy.php');
+include('/m23/inc/server.php');
+
 
 $xmlFile = $argv[1];
 array_shift($argv);
 $AutoTestO = new CAutoTest($xmlFile, $argv);
+
 $AutoTestO->run();
 ?>
