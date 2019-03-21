7ee8a4bcf3d10fcdd7d7305241fdd6f45b94657a
m23project <hhabermann@pc-kiel.de>
2019-03-22 00:34:44 +0100

    mdk-devel-backup from 2019-03-22 00:34

--- mdk/autoTest/1runTests.sh
+++ mdk/autoTest/1runTests.sh
@@ -23,18 +23,38 @@ desktop="$3"
 
 
 ./autoTest.php 1m23server-auf-debian-installieren.m23test Debian9-amd64 192.168.1.96
+
+./autoTest.php 1m23server-auf-debian-installieren.m23test Debian9-amd64 192.168.1.96; AT_M23_SSH_PASSWORD='test' TEST_M23_BASE_URL='https://god:m23@192.168.1.96/m23admin' ./autoTest.php 1m23client-distro-install.m23test aTm23sATstretchKDE stretch Debian8KdeFull
+
 ./autoTest.php 1m23server-auf-debian-installieren.m23test Debian9-i386 192.168.1.93
+
 AT_M23_SSH_PASSWORD='test' TEST_M23_BASE_URL='https://god:m23@192.168.1.93/m23admin' ./autoTest.php 1m23client-distro-install.m23test aTm23sATstretchKDE stretch Debian8KdeFull
 
+AT_M23_SSH_PASSWORD='test' TEST_M23_BASE_URL='https://god:m23@192.168.1.93/m23admin' ./autoTest.php 1m23client-distro-install.m23test aTm23TessaXfce 'LinuxMint 19.1 Tessa' Mint19Xfce
 
+AT_M23_SSH_PASSWORD='test' TEST_M23_BASE_URL='https://god:m23@192.168.1.93/m23admin' ./autoTest.php 1m23client-distro-install.m23test aTm23Budgie Ubuntu-Bionic UbuntuBudgie1804
+
+
+
+
+UCS 4.4
+=======
 AT_debug=1 ./autoTest.php 1m23server-auf-UCS-installieren.m23test 'UCS 4.4 - Lokales Testrepo' 192.168.1.144
 AT_M23_SSH_PASSWORD='test' TEST_M23_BASE_URL='https://god:m23@192.168.1.144/m23admin' ./autoTest.php 1m23client-distro-install.m23test aTm23sATstretchKDE stretch Debian8KdeFull
 
 
 TEST_TYPE=xmltest AT_debug=1 ./autoTest.php 1m23server-auf-UCS-installieren.m23test 'UCS 4.4 - Lokales Testrepo' 192.168.1.144
 
-./autoTest.php 1m23server-auf-UCS-installieren.m23test 'UCS 4.4 - Lokales Testrepo' 192.168.1.144; AT_M23_SSH_PASSWORD='test' TEST_M23_BASE_URL='https://god:m23@192.168.1.144/m23admin' ./autoTest.php 1m23client-distro-install.m23test aTm23sATstretchMate stretch Debian8Mate
+./autoTest.php 1m23server-auf-UCS-installieren.m23test 'UCS 4.4 - Lokales Testrepo' 192.168.1.144
+ 
+ AT_M23_SSH_PASSWORD='test' TEST_M23_BASE_URL='https://god:m23@192.168.1.144/m23admin' ./autoTest.php 1m23client-distro-install.m23test aTm23sATstretchMate stretch Debian8Mate
+ 
+AT_M23_SSH_PASSWORD='test' TEST_M23_BASE_URL='https://god:m23@192.168.1.144/m23admin' ./autoTest.php 1m23client-distro-install.m23test aTm23JessieGnome jessie Debian8GnomeFull
+
+AT_M23_SSH_PASSWORD='test' TEST_M23_BASE_URL='https://god:m23@192.168.1.144/m23admin' ./autoTest.php 1m23client-distro-install.m23test aTm23TessaCinnamon 'LinuxMint 19.1 Tessa' Mint19Cinnamon
+
+AT_M23_SSH_PASSWORD='test' TEST_M23_BASE_URL='https://god:m23@192.168.1.144/m23admin' ./autoTest.php 1m23client-distro-install.m23test aTm23Bionic Ubuntu-Bionic Ubuntudesktop1804
 
 
-./autoTest.php 1m23server-auf-debian-installieren.m23test Debian9-amd64 192.168.1.96; AT_M23_SSH_PASSWORD='test' TEST_M23_BASE_URL='https://god:m23@192.168.1.96/m23admin' ./autoTest.php 1m23client-distro-install.m23test aTm23sATstretchKDE stretch Debian8KdeFull
+
 
